2336c4e35fd83791c3721f8a33f043bc6b9c91c0
RysKowalski <ryskowalski0@gmail.com>
2025-06-02 21:07:07 +0200

    skibidi

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -4,15 +4,22 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-      style = "storm",          -- lub inna odmiana Tokyonight
+      style = "storm", -- lub inna odmiana Tokyonight
       transparent = false,
       on_highlights = function(hl, c)
         -- ustaw kolor dla @variable
         hl["@variable"] = { fg = "#9CDCFE" }
 
         hl["@Type"] = { fg = "#4EC9B0" }
+
+        hl["Function"] = { fg = "#ffb300" }
+
+        hl["@keyword"] = { fg = "#569CD6" }
+
+        hl["PreProc"] = { fg = "#569CD6" }
+
+        hl["@module"] = { fg = "#4EC9B0" }
       end,
     },
   },
 }
-
