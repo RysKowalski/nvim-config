3828a12df91e6ffa1c22fb0cb555595c209d8143
RysKowalski <ryskowalski0@gmail.com>
2025-06-15 00:05:06 +0200

    d

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,15 @@
 return {
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        size = 20,
+        open_mapping = [[<c-\>]],
+        direction = "horizontal",
+        start_in_insert = true,
+        persist_size = true,
+      })
+    end,
+  },
 }
